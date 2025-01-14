b6b27db6bfbb2a772301d60314327ab71e078398
yakatyansh <ykpetrolhead08@outlook.com>
2025-01-14 14:20:59 +0530

    added design elements

--- src/slices/ProductGrid/SkateboardProduct.tsx
+++ src/slices/ProductGrid/SkateboardProduct.tsx
@@ -1,19 +1,29 @@
+import { ButtonLink } from '@/components/ButtonLink';
+import { HorizontalLine, VerticalLine } from '@/components/Line';
 import { createClient } from '@/prismicio';
 import {Content, isFilled} from '@prismicio/client'
 import { PrismicNextImage } from '@prismicio/next';
+import clsx from 'clsx';
 import React from 'react'
 import { FaStar } from 'react-icons/fa6';
 
 type Props = {
     id: string;
-}
+};
+
+const VERTICAL_LINE_CLASSES = "absolute top-0 h-full stroke-2 text-stone-300 transition-colors group-hover:text-stone-400"
+const HORIZONTAL_LINE_CLASSES = "-mx-8 stroke-2 text-stone-300 transition-colors group-hover:text-stone-400"
 
 export async function SkateboardProduct({ id }: Props) {
     const client = createClient()
     const product =  await client.getByID<Content.SkateboardDocument>(id)
     const price = isFilled.number(product.data.price)? `$${(product.data.price/100).toFixed(2)}`:"Price Not Available"
   return (
-    <div>
+    <div className="group relative mx-auto w-full max-w-72 px-8 pt-4 ">
+        <VerticalLine className={clsx(VERTICAL_LINE_CLASSES,"left-4")}/>
+        <VerticalLine className={clsx(VERTICAL_LINE_CLASSES,"right-4")}/>
+        <HorizontalLine className={HORIZONTAL_LINE_CLASSES}/>
+        
         <div className="flex items-center justify-between ~text-sm/2xl">
             <span>{price}</span>
             <span className="inline-flex items-center gap-1">
@@ -21,7 +31,14 @@ export async function SkateboardProduct({ id }: Props) {
             </span>
         </div>
         <div className="-mb-1 overflow-hidden py-4">
-        <PrismicNextImage alt="" field={product.data.image} width={150}/>
+        <PrismicNextImage alt="" field={product.data.image} width={150} className="mx-auto w-[58%] origin-top transform-gpu transition-transform duration-500 ease-in-out 
+        group-hover:scale-150"/>
+        </div>
+        <h3 className="my-2 text-center font-sans leading-tight ~text-lg/xl">{product.data.name}
+        </h3>
+
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-200 group-hover:opacity-100">
+            <ButtonLink field={product.data.customizer_link}>Customize</ButtonLink>
         </div>
     </div>
   )
