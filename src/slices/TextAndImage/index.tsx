e9e4f21cab3584accc2a448bf11137d740c4f367
yakatyansh <ykpetrolhead08@outlook.com>
2025-01-18 23:51:36 +0530

    more text-image work

--- src/slices/TextAndImage/index.tsx
+++ src/slices/TextAndImage/index.tsx
@@ -1,7 +1,10 @@
 import { Bounded } from "@/components/Bounded";
+import { ButtonLink } from "@/components/ButtonLink";
+import { Heading } from "@/components/Heading";
 import { Content } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
+import clsx from "clsx";
 import { JSX } from "react";
 
 /**
@@ -9,22 +12,44 @@ import { JSX } from "react";
  */
 export type TextAndImageProps = SliceComponentProps<Content.TextAndImageSlice>;
 
+
 /**
  * Component for "TextAndImage" Slices.
  */
 const TextAndImage = ({ slice }: TextAndImageProps): JSX.Element => {
+  const theme = slice.primary.theme
   return (
     <Bounded
       data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}    
+      data-slice-variation={slice.variation}  
+      className={clsx(
+            theme === "Blue" && "bg-texture bg-brand-blue text-white",
+            theme === "Orange" && "bg-texture bg-brand-orange text-white",
+            theme === "Navy" && "bg-texture bg-brand-navy text-white",
+            theme === "Lime" && "bg-texture bg-brand-lime ",
+      )}  
       >
-        {slice.primary.theme}
+        <div className="grid grid-cols-1 items-center gap-12 md:grid-cols-2 md:gap-24">
+          <div className={clsx(
+            "flex flex-col items-center gap-8 text-center md:items-start md:text-left",
+            slice.variation === "imageOnLeft" && "md:order-2"
+          )
+          }>
+        <Heading size="lg" as="h2">
         <PrismicText field={slice.primary.heading} />
+        </Heading>
+        <div className="max-w-md text-lg leading-relaxed">
         <PrismicRichText field={slice.primary.body} />
-        <PrismicNextLink field={slice.primary.button} />
-        <PrismicNextImage field={slice.primary.background_image} />
+        </div>
+        <ButtonLink field={slice.primary.button} color={theme === "Lime" ? "orange":"lime"} >
+          {slice.primary.button.text}
+          </ButtonLink>
+        </div>
+          
+        {/* <PrismicNextImage field={slice.primary.background_image} /> */}
         <PrismicNextImage field={slice.primary.foreground_image} />
 
+        </div>
 
      
     </Bounded>
